087a77d7e43fd344d3cec4376c9e2899b778fa38
shanzm <2351144690@qq.com>
2019-04-25 00:56:34 +0800

    2019年4月25日 00:56:14-update 分页

--- yzk/HuaGongWeb/Admin/ProductList.ashx.cs
+++ yzk/HuaGongWeb/Admin/ProductList.ashx.cs
@@ -17,9 +17,47 @@ namespace HuaGongWeb.Admin
         {
             context.Response.ContentType = "text/html";
 
-            string sql = "select p.Id as Id, p.Name as Name,p.Msg as Msg,pc.Name as CategoryName from T_Products as p left  join T_ProductCategories as pc on p.CategoryId=pc.Id";
-            DataTable dtProduct = SqlHelper.GetDataTable(sql, CommandType.Text);
-            var data = new { Products = dtProduct.Rows, Title = "产品展示" };
+            int PageNum = 1;//页数默认是1，则star=（1-1）*10，end=1*10
+            if (context.Request["PageNum"] != null)//接收的页码按钮超链接中的PageNum
+            {
+                PageNum = Convert.ToInt32(context.Request["PageNum"]);
+            }
+
+            string sql = @"select Id, Name,CategoryName
+                          from
+                          (select row_number() over(order by p.Id) as num,p.Id, p.Name as Name,pc.Name as CategoryName 
+                          from T_Products as p left  join T_ProductCategories as pc 
+                          on p.CategoryId=pc.Id ) as T
+                          where T.num between @start and @end";
+
+
+            SqlParameter[] param =
+             {
+                new SqlParameter  ("@start",(PageNum -1)*10),
+                new SqlParameter ("@end",PageNum*10)
+            };
+            DataTable dtProduct = SqlHelper.GetDataTable(sql, CommandType.Text, param);
+
+
+            //分页的页码
+
+            //表中数据总行数
+            int totalCount = (int)SqlHelper.ExecuteScalar("select count(*) from T_Products", CommandType.Text);
+            //分页数
+            int pageCount = (int)Math.Ceiling(totalCount / 10.0);//10行一页
+                                                                 //定义一个页码数组
+            object[] pageDate = new object[pageCount];
+
+            for (int i = 0; i < pageDate.Length; i++)//for (int = 0; i<pageCount; i++)
+            {
+                pageDate[i] = new { Href = "ProductList.ashx?PageNum=" + (i + 1), PageName = "第" + (i + 1) + "页" };
+            }
+
+
+
+
+
+            var data = new { Products = dtProduct.Rows, Title = "产品展示", Page = pageDate };
 
             string html = NVelocityHelper.RenderHtml("Admin/ProductList.html", data);
 
